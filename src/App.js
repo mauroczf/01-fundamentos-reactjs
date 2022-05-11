be6fec238ff03d20d5e5d57c04fb2d593b1281c6
mauroczf <mauroczf@gmail.com>
2022-05-11 15:19:20 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from "react";
+import api from './services/api';
+import "./styles.css";
+
+function App() {
+
+  const [project, setProject] = useState([]);
+
+  useEffect(() => {
+    loadApi()
+  }, []);
+
+
+  async function handleAddRepository() {
+    const response = await api.post(`repositories`, {
+      title: "Desafio React BootCamp RocketSeat",
+      techs: ["React", "ReactJS", "NodeJS"],
+      url: "https://github.com/Delfio/desafio-conceitos-reactjs"
+    })
+    setProject([...project, response.data]);
+  }
+
+  async function loadApi() {
+    const response = await api.get(`/repositories`);
+    setProject(response.data);
+  }
+
+  async function handleRemoveRepository(id) {
+    // await api.delete(`/repositories/${id}`);
+    // handleAddRepository();
+    setProject([]);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {project.map((el, index) => (
+          <li key={el.id}>
+            {el.title}
+
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => handleRemoveRepository()}>
+        Remover
+      </button>
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
